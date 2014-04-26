a162de7bc65a85988acaceeff000ea2604cfa3a0
Benjamin Bouvier <benj@benj.me>
2014-04-26 18:52:45 +0200

    bnjify: install colout in .files/bin/

--- bnjify.sh
+++ bnjify.sh
@@ -8,6 +8,10 @@ ln -s ~/.files/.bundles.vim ~/.bundles.vim
 ln -s ~/.files/.hgrc ~/.hgrc
 ln -s ~/.files/redshift.conf ~/.config/redshift.conf
 
+mkdir -p ~/.files/bin
+git clone https://github.com/nojhan/colout ~/.files/bin/colout
+ln -s ~/.files/bin/colout/colout/colout.py ~/.files/colout
+
 # install oh-my-zsh in the .files directory
 curl -L http://install.ohmyz.sh | ZSH=~/.files/zsh sh
 sudo chsh -s /bin/zsh
