c01b9d3f935392f7cb7e2d15290eca155c00c58e
Benjamin Bouvier <public@benj.me>
2014-05-01 00:58:46 +0200

    unified directory for hg extensions

--- bnjify.sh
+++ bnjify.sh
@@ -1,4 +1,4 @@
-sudo apt-get install -y vim-gnome zsh build-essential curl redshift git python3-pygments
+sudo apt-get install -y vim-gnome zsh build-essential curl redshift git python3-pygments mercurial
 
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
@@ -11,6 +11,9 @@ ln -s ~/.files/redshift.conf ~/.config/redshift.conf
 mkdir -p ~/.files/bin
 git clone https://github.com/nojhan/colout ~/.files/bin/colout
 ln -s ~/.files/bin/colout/colout/colout.py ~/.files/colout
+
+hg clone https://bitbucket.org/edgimar/crecord ~/.files/bin/crecord
+hg clone https://hg.mozilla.org/hgcustom/version-control-tools ~/.files/bin/version-control-tools
 
 # install oh-my-zsh in the .files directory
 curl -L http://install.ohmyz.sh | ZSH=~/.files/zsh sh
