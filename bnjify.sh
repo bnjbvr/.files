d58fdf1360e128baeefd9d592ef4f8a0df2655ce
Benjamin Bouvier <public@benj.me>
2014-12-30 19:45:53 +0100

    Move conf files to conf/

--- bnjify.sh
+++ bnjify.sh
@@ -2,11 +2,11 @@ sudo apt-get install -y vim-gnome zsh build-essential curl redshift git python3-
 
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-ln -s ~/.files/.zshrc ~/.zshrc
-ln -s ~/.files/.vimrc ~/.vimrc
-ln -s ~/.files/.bundles.vim ~/.bundles.vim
-ln -s ~/.files/.hgrc ~/.hgrc
-ln -s ~/.files/redshift.conf ~/.config/redshift.conf
+ln -s ~/.files/conf/zshrc ~/.zshrc
+ln -s ~/.files/conf/vimrc ~/.vimrc
+ln -s ~/.files/conf/bundles.vim ~/.bundles.vim
+ln -s ~/.files/conf/hgrc ~/.hgrc
+ln -s ~/.files/conf/redshift.conf ~/.config/redshift.conf
 
 git clone https://github.com/sstangl/iongraph.git ~/.files/bin/iongraph
 
