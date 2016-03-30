a106528aa17112a5c1c843882c2e7a1e23e7a962
Benjamin Bouvier <public@benj.me>
2016-03-30 18:28:31 +0200

    shot/upshot

--- bin/upshot.sh
+++ bin/upshot.sh
@@ -9,4 +9,11 @@ then
 fi
 
 scrot -s -q 100 /tmp/$filename
-echo /tmp/$filename
+cd /tmp/ && up $filename
+
+if [ $? = "0" ]
+then
+  rm /tmp/$filename
+else
+  echo "Could not upload the file /tmp/$filename"
+fi
