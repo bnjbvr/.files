07b1c651f3aa737ee4cdb4f8ed47104bd0320ac4
Benjamin Bouvier <public@benj.me>
2017-02-02 14:33:27 +0100

    inboundstatus.py: update to latest API;

--- bin/inboundstatus.py
+++ bin/inboundstatus.py
@@ -11,19 +11,34 @@ To the extent possible under law, Benjamin Bouvier has waived all copyright and 
 import urllib2
 import json
 
-URL = 'https://treestatus.mozilla.org/mozilla-inbound?format=json'
+URL = 'https://treestatus.mozilla.org/trees/mozilla-inbound'
 
 red_start = '\033[2;31m'
 green_start = '\033[2;32m'
 end = '\033[0m'
 
-try:
-    req = urllib2.Request(url=URL)
-    f = urllib2.urlopen(req)
-    r = json.loads(f.read()).get('status', None)
-    color = red_start
-    if r == 'open':
-        color = green_start
-    print color + r.upper() + end
-except Exception as e:
-    print 'Error: ', e
+def main():
+    try:
+        req = urllib2.Request(url=URL)
+        f = urllib2.urlopen(req)
+        r = json.loads(f.read()).get('result', None)
+    except Exception as e:
+        print 'Error: ', e
+        return
+
+    if r is None:
+        print "The API seems to have changed: 'result' doesn't exist anymore."
+        return
+
+    status = r.get('status', '?')
+
+    color = green_start if status == 'open' else red_start
+
+    msg = status.upper()
+    if msg != 'OPEN':
+        msg += '\n' + r.get('reason', '(undocumented reason)')
+
+    print color + msg + end
+
+if __name__ == '__main__':
+    main()
