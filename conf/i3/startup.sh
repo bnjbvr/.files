f163016bb3556eca51ee2d515ac4503f84cd6818
Benjamin Bouvier <public@benj.me>
2019-08-14 11:43:59 +0200

    i3: put everything under its own directory + background images;

--- conf/i3/startup.sh
+++ conf/i3/startup.sh
@@ -3,8 +3,11 @@
 if [ "$(hostname)" == "xps-linux" ]; then
     # XPS-Linux.
     xrandr --output eDP1 --mode 1920x1080
+elif [ "$(hostname)" == "benjamin-bertja" ]; then
+    # Grosse bertja!
+    feh --bg-scale /home/ben/.files/conf/i3/bertja-1.jpg /home/ben/.files/conf/i3/bertja-2.jpg
 else
-    notify-send "i3specific.sh: I don't know this machine!"
+    notify-send "i3/startup.sh: I don't know this machine!"
 fi
 
 # On every restart, try to set a favorite keyboard layout.
--- conf/i3/status.sh
+++ conf/i3/status.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 if [ -e "/home/ben/code/i3status-rust/target/release/i3status-rs" ]; then
     hostname=$(hostname)
-    if [ -e "/home/ben/.files/conf/i3-statusbar-$hostname.toml" ]; then
+    if [ -e "/home/ben/.files/conf/i3/statusbar-$hostname.toml" ]; then
         ~/code/i3status-rust/target/release/i3status-rs \
-            ~/.files/conf/i3-statusbar-$(hostname).toml
+            ~/.files/conf/i3/statusbar-$(hostname).toml
     else
         i3status
     fi
