b60032862af2cfd0cc62a3658677bb22d6af772e
Benjamin Bouvier <public@benj.me>
2019-08-26 20:05:38 +0200

    i3: wallpaper for xps;

--- conf/i3/startup.sh
+++ conf/i3/startup.sh
@@ -3,6 +3,7 @@
 if [ "$(hostname)" == "xps-linux" ]; then
     # XPS-Linux.
     xrandr --output eDP1 --mode 1920x1080
+    feh --bg-scale /home/ben/.files/conf/i3/xps.png
 elif [ "$(hostname)" == "benjamin-bertja" ]; then
     # Grosse bertja!
     feh --bg-scale /home/ben/.files/conf/i3/bertja-1.jpg /home/ben/.files/conf/i3/bertja-2.jpg
