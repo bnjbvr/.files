5631785e5a2fdc1cd570b5570524fb3ffa0fe47a
Benjamin Bouvier <public@benj.me>
2022-09-29 16:56:59 +0200

    Autoload vscode snippets if detected

--- conf/vim/lua/plugins_config.lua
+++ conf/vim/lua/plugins_config.lua
@@ -19,6 +19,21 @@ require("luasnip.loaders.from_snipmate").lazy_load({
     paths = "~/.files/conf/vim/snips/"
 })
 
+local function maybe_load_vscode_snippets()
+    local Path = require("luasnip.util.path")
+    local cur_dir = vim.fn.getcwd()
+    local vscode_dir = Path.join(cur_dir, ".vscode")
+    local package_json_file = Path.join(vscode_dir, "package.json")
+    if Path.exists(package_json_file)
+    then
+        require("luasnip.loaders.from_vscode").lazy_load({
+            paths = vscode_dir
+        })
+    end
+end
+
+maybe_load_vscode_snippets()
+
 -- press <Tab> to expand or jump in a snippet. These can also be mapped separately
 -- via <Plug>luasnip-expand-snippet and <Plug>luasnip-jump-next.
 --helpers.map('i', '')
