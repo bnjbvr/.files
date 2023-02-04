81ed9f387a6287e8503c15b54f156756b61bac31
Benjamin Bouvier <public@benj.me>
2023-02-04 12:33:28 +0100

    vim: open nvim-tree in a floating window centered on screen

--- conf/vim/lua/plugins_config.lua
+++ conf/vim/lua/plugins_config.lua
@@ -6,7 +6,40 @@ require('moonlight').set()
 
 -- *************
 -- File tree
-require("nvim-tree").setup()
+
+local HEIGHT_RATIO = 0.8
+local WIDTH_RATIO = 0.5
+
+-- From nvim-tree recipes https://github.com/nvim-tree/nvim-tree.lua/wiki/Recipes#center-a-floating-nvim-tree-window
+require('nvim-tree').setup({
+  view = {
+    float = {
+      enable = true,
+      open_win_config = function()
+        local screen_w = vim.opt.columns:get()
+        local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+        local window_w = screen_w * WIDTH_RATIO
+        local window_h = screen_h * HEIGHT_RATIO
+        local window_w_int = math.floor(window_w)
+        local window_h_int = math.floor(window_h)
+        local center_x = (screen_w - window_w) / 2
+        local center_y = ((vim.opt.lines:get() - window_h) / 2)
+                         - vim.opt.cmdheight:get()
+        return {
+          border = 'rounded',
+          relative = 'editor',
+          row = center_y,
+          col = center_x,
+          width = window_w_int,
+          height = window_h_int,
+        }
+        end,
+    },
+    width = function()
+      return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
+    end,
+  },
+})
 
 -- *************
 -- Quick search with s{char1}{char2}
