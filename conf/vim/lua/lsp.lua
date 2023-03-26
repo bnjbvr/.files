7f078ff5acdad2f9a19423a3c0a0c07c1f145479
Benjamin Bouvier <public@benj.me>
2023-03-26 23:20:27 +0200

    rust lsp: use clippy instead of check

--- conf/vim/lua/lsp.lua
+++ conf/vim/lua/lsp.lua
@@ -56,6 +56,9 @@ require("mason-lspconfig").setup_handlers {
                         },
                         procMacro = {
                             enable = true
+                        },
+                        check = {
+                            command = "clippy"
                         }
                     }
                 },
