3da524471248e6140182f1aced5527206df11aff
Benjamin Bouvier <benjamin@bouvier.cc>
2025-03-31 13:22:18 +0200

    wezterm: different configurations based on hostname

--- conf/wezterm.lua
+++ conf/wezterm.lua
@@ -1,10 +1,33 @@
 local wezterm = require 'wezterm'
 
+function hostname()
+    local handle = io.open("/etc/hostname", "r")
+    local result = handle:read()
+    handle:close()
+    return result:gsub("\n$", "")
+end
+
+local use_big_screen = false
+local is_linux = false
+
+local status, hostname = pcall(hostname)
+
+wezterm.log_info("Status: " .. tostring(status) .. "/ Hostname: " .. hostname)
+
+if status then
+    use_big_screen = hostname == "archlinux"
+    is_linux = hostname == "archlinux"
+end
+
 -- Start wezterm full-screen.
 local mux = wezterm.mux
 wezterm.on("gui-startup", function()
   local tab, pane, window = mux.spawn_window{}
-  window:gui_window():toggle_fullscreen()
+  if is_linux then
+      window:gui_window():maximize()
+  else
+      window:gui_window():toggle_fullscreen() -- nice on MacOS, not on Linux
+  end
 end)
 
 local config = wezterm.config_builder()
@@ -23,13 +46,23 @@ config.color_scheme = 'cyberpunk'
 config.window_background_opacity = 0.85
 
 -- Default program run at start.
-config.default_prog = { '/opt/homebrew/bin/zellij' }
+if is_linux then
+    config.default_prog = { 'zellij' }
+    config.window_decorations = "RESIZE"
+else
+    config.default_prog = { '/opt/homebrew/bin/zellij' }
+end
 
 -- Hide the tab bar; I'm using zellij :)
 config.enable_tab_bar = false
 
 -- Cool fonts
 config.font = wezterm.font 'FiraCode Nerd Font'
-config.font_size = 14
+
+if use_big_screen then
+    config.font_size = 18
+else
+    config.font_size = 14
+end
 
 return config
