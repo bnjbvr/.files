7f8ebce5c5295e3bd5ef99f8391e02e390740da9
Benjamin Bouvier <benjamin@bouvier.cc>
2025-09-18 11:14:03 +0200

    fish: make the alias d and dc use docker or podman based on which is installed

--- conf/fish/functions/d.fish
+++ conf/fish/functions/d.fish
@@ -1,4 +1,11 @@
-function d --wraps=docker --description 'alias d=docker'
-  docker $argv
-        
+function d --wraps=docker --wraps=podman --description 'alias d=docker'
+    # Use docker if available, otherwise use podman.
+    if command -v docker > /dev/null
+        docker $argv
+    else if command -v podman > /dev/null
+        podman $argv
+    else
+        echo "Error: Neither docker nor podman is installed." >&2
+        return 1
+    end
 end
--- conf/fish/functions/dc.fish
+++ conf/fish/functions/dc.fish
@@ -1,3 +1,11 @@
-function dc --wraps=docker-compose --description 'alias dc=docker-compose'
-  docker-compose $argv
+function dc --wraps=docker-compose --wraps=podman-compose --description 'alias dc=podman-compose'
+    # Use "docker compose" if available, otherwise use podman.
+    if command -v docker > /dev/null
+        docker compose $argv
+    else if command -v podman-compose > /dev/null
+        podman-compose $argv
+    else
+        echo "Error: Neither docker-compose nor podman-compose is installed." >&2
+        return 1
+    end
 end
